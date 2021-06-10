95aa9f38f0962b2247153a89ef4830dd2cc1019f
MichalBurdynski <micburdy@wp.pl>
2021-06-10 20:47:21 +0200

    Zadanie 1

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,10 +1,60 @@
 package com.company;
 
 import java.io.File;
+import java.util.Objects;
 
 public class Animal {
-    String species;
-    Double weight;
+    final String species;
+    private Double weight;
     String name;
     File pic;
+
+    //constant values of animal weight and amount of food its eats
+    static final public Double DEFAULT_ANIMAL_WEIGHT = 1.0;
+
+    //Constructor with parameter species that depending on its value setting appropriate value to field weight
+    Animal(String species)
+    {
+        this.species = species;
+        if (Objects.equals(this.species, "pies")) {
+            this.weight = 10.0;
+        }
+        else if (Objects.equals(this.species, "kot")) {
+            this.weight = 2.0;
+        }
+        else
+        {
+            this.weight = DEFAULT_ANIMAL_WEIGHT;
+        }
+    }
+
+    /* Implementation of feed method.
+       The method takes as parameter default amount of food to increase an animal weight. */
+    public void feed()
+    {
+        if (this.weight <= 0)
+        {
+            System.out.println("Zwierze nie żyje");
+        }
+        else
+        {
+            this.weight += 1;
+            System.out.println("Zwierze nakarmione. Masa zwierzęcia wynosi: "+this.weight);
+        }
+    }
+
+    //takeForAWalk method This method decreases animal weight by 3.0.
+    public void takeForAWalk()
+    {
+        if (this.weight <= 0.0)
+        {
+            System.out.println("Zwierze nie żyje");
+        }
+        else
+        {
+            this.weight -= 3.0;
+            System.out.println("Zwierze wyprowadzone. Masa zwierzęcia wynosi: "+this.weight);
+        }
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,52 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+// Creating Human class objects
+        Human human1 = new Human();
+        human1.firstName = "Andrzej";
+        human1.lastName = "Nowak";
+        human1.pet = new Animal("pies");
+        human1.phone = "Iphone";
+
+        // Creating Animal class objects
+        Animal dog1 = new Animal("pies");
+        dog1.name = "Azorek";
+        Animal cat1 = new Animal("kot");
+        cat1.name = "Puszek";
+        Animal crocodile1 = new Animal("krokodyl");
+        crocodile1.name = "Zebus";
+
+        //Testing feed and takeForAWalk method
+
+        //dog1
+        dog1.feed();
+        dog1.feed();
+        dog1.takeForAWalk();
+        dog1.takeForAWalk();
+        dog1.takeForAWalk();
+        dog1.takeForAWalk();
+        dog1.takeForAWalk();
+        dog1.feed();
+        dog1.takeForAWalk();
+
+        System.out.println();
+
+        //cat1
+
+        cat1.feed();
+        cat1.takeForAWalk();
+        cat1.takeForAWalk();
+        cat1.feed();
+        cat1.takeForAWalk();
+
+        System.out.println();
+
+        //crocodile1
+        crocodile1.feed();
+        crocodile1.takeForAWalk();
+        crocodile1.takeForAWalk();
+        crocodile1.takeForAWalk();
+        crocodile1.takeForAWalk();
+        crocodile1.feed();
     }
 }
